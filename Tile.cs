eb85a2e07ebfa3f5b1682d904c5b1fd2bd49f85c
Durim Miziraj <durim.miziraj@gmail.com>
2021-09-23 12:59:55 +0200

    Created a wayfinder class.

---
+++ Tile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Group1Game
+{
+    class Tile
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public int TimesVisited { get; set; }
+
+        public Tile(int col, int row, int timesVisited)
+        {
+            Row = row;
+            Col = col;
+            TimesVisited = timesVisited;
+        }
+    }
+}
--- WayFinder.cs
+++ WayFinder.cs
@@ -1,8 +1,9 @@
 ﻿/*
- * Klassen förutsätter att spelaren startar från nedersta hörnet åt vänster.
- * 
+ * Endast "AreEdgesClear()" kan nås av andra klasser.
+ * Den tar en spelkarta i form av string[,] och en array av hinder.
+ * Den returnerar en ett boll värde beroende på om alla kanter är fria från hinder eller ej.
+ * Durim står bakom denna klass och tar fullt ansvar för den.
  */
-
 
 using System;
 using System.Collections.Generic;
@@ -10,47 +11,87 @@ using System.Text;
 
 namespace Group1Game
 {
-    class WayFinder
+    static class WayFinder
     {
-        public string[,] Map { get; set; }
+        private static string[,] grid;
 
-        public string Obstacle { get; set; } = "O";
+        private static int rowAmount = grid.GetLength(0);
 
-        public void MoveDown()
+        private static int colAmount = grid.GetLength(1);
+
+        private static string[] obstaclesArr;
+
+        private static bool IsRightColFree()
         {
+            bool result = true;                                                             // __________________
+                                                                                            // | | | | | | | | |·|
+            for ( int row = 0; row < rowAmount; row++ )                                     // | | | | | | | | |·|
+            {                                                                               // | | | | | | | | |·|
+                for (int i = 0; i < obstaclesArr.Length; i++)                               // | | | | | | | | |·|
+                {                                                                           // | | | | | | | | |·|
+                    if (grid[row, rowAmount] == obstaclesArr[i]) { result = false; break; } // | | | | | | | | |·|
+                }                                                                           // | | | | | | | | |·|
+            }
 
+            return result;
         }
 
-        public void MoveRight()
+        private static bool IsLeftColFree()
         {
+            bool result = true;                                                         // __________________
+                                                                                        // |·| | | | | | | | |
+            for ( int row = 0; row < rowAmount; row++)                                  // |·| | | | | | | | |
+            {                                                                           // |·| | | | | | | | |
+                for (int i = 0; i < obstaclesArr.Length; i++)                           // |·| | | | | | | | |
+                {                                                                       // |·| | | | | | | | |
+                    if (grid[row, 0] == obstaclesArr[i]) { result = false; break; }     // |·| | | | | | | | |
+                }                                                                       // |·| | | | | | | | |
+            }
 
+            return result;
         }
 
-        public void MoveUp()
-        {
+        private static bool IsLowerRowFree()                                            // __________________
+        {                                                                               // | | | | | | | | | |
+            bool result = true;                                                         // | | | | | | | | | |
+                                                                                        // | | | | | | | | | |
+            for ( int col = 0; col < colAmount; col++)                                  // | | | | | | | | | |
+            {                                                                           // | | | | | | | | | |
+                for (int i = 0; i < obstaclesArr.Length; i++)                           // | | | | | | | | | |
+                {                                                                       // |·|·|·|·|·|·|·|·|·|
+                    if (grid[col, rowAmount] == obstaclesArr[i]) { result = false; break; }
+                }
+            }
 
+            return result;
         }
 
-        public void CheckDown()
-        {
+        private static bool IsUpperRowFree()                                            // __________________
+        {                                                                               // |·|·|·|·|·|·|·|·|·|
+            bool result = true;                                                         // | | | | | | | | | |
+                                                                                        // | | | | | | | | | |
+            for (int col = 0; col < colAmount; col++)                                   // | | | | | | | | | |
+            {                                                                           // | | | | | | | | | |
+                for (int i = 0; i < obstaclesArr.Length; i++)                           // | | | | | | | | | |
+                {                                                                       // | | | | | | | | | |
+                    if (grid[col, 0] == obstaclesArr[i]) { result = false; break; }     // | | | | | | | | | |
+                }
+            }
 
+            return result;
         }
 
-        public void CheckRight()
+        public static bool AreEdgesClear(string[,] twoDimArray, string[] obstacles)
         {
+            obstaclesArr = obstacles;
+            grid = twoDimArray;
 
+            if ( IsUpperRowFree() && IsLowerRowFree() && IsLeftColFree() && IsRightColFree() )
+            {
+                return true;
+            }
+
+            return false;
         }
-
-        public void CheckUpp()
-        {
-
-        }
-
-        public WayFinder(string[,] map)
-        {
-            Map = map;
-        }
-
     }
-
 }
